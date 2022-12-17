d3be86e0a390d905fe4b5fb4cd3076a8abe5209a
Rafael Sundorf <rafael.sundorf@home2share.de>
2022-12-17 09:41:55 +0100

    Reformats file

--- src/main.rs
+++ src/main.rs
@@ -3,58 +3,58 @@ use std::io::BufRead;
 fn read_dir(path: &str, include_hidden: bool) -> u128 {
     match std::fs::read_dir(path) {
         Ok(res) => {
-            match res.map(|entry| {
-                match entry {
-                    Ok(ent) => {
-                        match ent.file_type() {
-                            Ok(typ) => {
-                                if typ.is_dir() {
-                                    if (ent.file_name().to_str().unwrap().starts_with(".") && include_hidden) || !ent.file_name().to_str().unwrap().starts_with(".")  {
-                                        read_dir(ent.path().to_str().unwrap(), include_hidden)
-                                    } else {
-                                        0 as u128
-                                    }
-                                } else if typ.is_file() {
-                                    if ent.file_name().to_str().unwrap().starts_with(".") && !include_hidden {
-                                        return 0 as u128
-                                    }
-                                    match std::fs::File::open(ent.path().to_str().unwrap()) {
-                                        Ok(file) => {
-                                            let reader = std::io::BufReader::new(file);
-                                            let mut lines = 0 as u128;
-                                            for line in reader.lines() {
-                                                match line {
-                                                    Ok(_) => lines += 1 as u128,
-                                                    Err(_) => {
-                                                        return 0 as u128;
-                                                    }
-                                                }
-                                            }
-                                            lines
-                                        },
-                                        Err(e) => {
-                                            eprintln!("{}", e);
-                                            0 as u128
-                                        }
-                                    }
+            match res
+                .map(|entry| match entry {
+                    Ok(ent) => match ent.file_type() {
+                        Ok(typ) => {
+                            if typ.is_dir() {
+                                if (ent.file_name().to_str().unwrap().starts_with(".")
+                                    && include_hidden)
+                                    || !ent.file_name().to_str().unwrap().starts_with(".")
+                                {
+                                    read_dir(ent.path().to_str().unwrap(), include_hidden)
                                 } else {
                                     0 as u128
                                 }
-                            },
-                            Err(_) => 0 as u128
+                            } else if typ.is_file() {
+                                if ent.file_name().to_str().unwrap().starts_with(".")
+                                    && !include_hidden
+                                {
+                                    return 0 as u128;
+                                }
+                                match std::fs::File::open(ent.path().to_str().unwrap()) {
+                                    Ok(file) => {
+                                        let reader = std::io::BufReader::new(file);
+                                        let mut lines = 0 as u128;
+                                        for line in reader.lines() {
+                                            match line {
+                                                Ok(_) => lines += 1 as u128,
+                                                Err(_) => {
+                                                    return 0 as u128;
+                                                }
+                                            }
+                                        }
+                                        lines
+                                    }
+                                    Err(e) => {
+                                        eprintln!("{}", e);
+                                        0 as u128
+                                    }
+                                }
+                            } else {
+                                0 as u128
+                            }
                         }
+                        Err(_) => 0 as u128,
                     },
-                    Err(_) => {
-                        0 as u128
-                    }
-                }
-            }).reduce(|a, b| {
-                a + b
-            }) {
+                    Err(_) => 0 as u128,
+                })
+                .reduce(|a, b| a + b)
+            {
                 Some(val) => val,
-                None => 0 as u128
+                None => 0 as u128,
             }
-        },
+        }
         Err(e) => {
             eprintln!("Error while reading directory {}: {}", path, e);
             0 as u128
@@ -65,8 +65,20 @@ fn read_dir(path: &str, include_hidden: bool) -> u128 {
 fn main() {
     let args = clap::Command::new("line_count")
         .author("Rafael Sundorf <rafael.sundorf@gmail.com>")
-        .arg(clap::Arg::new("location").short('l').long("location").help("The directory path to loop through or file").default_value("."))
-        .arg(clap::Arg::new("include-hidden").short('i').long("include-hidden").help("Includes hidden directories").num_args(0..1))
+        .arg(
+            clap::Arg::new("location")
+                .short('l')
+                .long("location")
+                .help("The directory path to loop through or file")
+                .default_value("."),
+        )
+        .arg(
+            clap::Arg::new("include-hidden")
+                .short('i')
+                .long("include-hidden")
+                .help("Includes hidden directories")
+                .num_args(0..1),
+        )
         .get_matches();
 
     let path: &String = args.get_one("location").unwrap();
